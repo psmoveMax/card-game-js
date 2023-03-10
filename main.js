51a0e1c996cdf6784b8048ac2ab45ea23622a40c
Max Avvakumov <zerotomastery97@gmail.com>
2023-03-10 17:43:12 +0600

    tech update

--- main.js
+++ main.js
@@ -47,7 +47,7 @@
         let inside_cards = document.querySelectorAll(".inside");
         // Проверка на совпадение
         if (inside_cards.length == 2) {
-          setTimeout(checkMatch, 150);
+          setTimeout(checkMatch, 250);
 
           function checkMatch() {
             game.classList.add("block_game");
@@ -73,6 +73,14 @@
               document.querySelectorAll(".card").length ==
               document.querySelectorAll(".guessed").length
             ) {
+              if (document.querySelector('#timer') != null) {
+                document.querySelector('#timer').remove();
+              }
+
+              let deleteElement = document.querySelectorAll('.card');
+              for (let i = 0; i < deleteElement.length; i++) {
+                deleteElement[i].remove();
+              }
               console.log("Winner");
               modal_winner.classList.add("modal_vis"); // добавляем видимость окна
               modal_winner.classList.remove("bounceOutDown"); // удаляем эффект закрытия
@@ -89,11 +97,55 @@
   // Этап 3. Используйте две созданные функции для создания массива перемешанными номерами. На основе этого массива вы можете создать DOM-элементы карточек.
   // У каждой карточки будет свой номер из массива произвольных чисел. Вы также можете создать для этого специальную функцию. count - количество пар.
 
-  function startGame(count, container) {
+
+
+  function startGame(count, timer, container) {
+
+    function createTimer() {
+      let timer_div = document.createElement("div");
+      timer_div.setAttribute('id', 'timer');
+      let seconds = 1000 * 5; //1000 = 1 second in JS
+      let timer;
+
+      timer_div.addEventListener("keypress", timer_work());
+
+      function timer_work() {
+        timer_div.removeEventListener("keypress", timer_work);
+        if (seconds == 5000)
+          timer = setInterval(timer_work, 1000)
+        seconds -= 1000;
+        timer_div.innerHTML = seconds / 1000 + " секунд";
+        if (seconds <= 0) {
+          clearInterval(timer);
+
+          if (document.querySelector('#timer') != null) {
+            document.querySelector('#timer').remove();
+          }
+
+          let deleteElement = document.querySelectorAll('.card');
+          for (let i = 0; i < deleteElement.length; i++) {
+            deleteElement[i].remove();
+          }
+          console.log("Loss");
+          modal_loss.classList.add("modal_vis"); // добавляем видимость окна
+          modal_loss.classList.remove("bounceOutDown"); // удаляем эффект закрытия
+        }
+
+      } //If seconds are equal or greater than 0, countdown until 1 minute has passed
+      //Else, clear the timer and alert user of how many words they type per minute
+
+      timer_div.innerHTML = seconds / 1000 + " секунд";
+      return timer_div;
+    }
+
     if (count % 2 == 0 && count > 0 && count < 11) {
       let shuffleArray = shuffle(createNumbersArray(count));
       let card;
       let startGame = false;
+      console.log(timer);
+      //Таймер
+
+
 
       // Формирование карт
       for (let i = 0; i < shuffleArray.length; i++) {
@@ -116,19 +168,24 @@
         }
 
         function startGameStarted() {
+          if (timer == true) {
+            timer_active = createTimer();
+            container.append(timer_active);
+          }
           for (let i = 0; i < shuffleArray.length; i++) {
             document.querySelectorAll(".card")[i].textContent = "";
             document.querySelectorAll(".card")[i].classList.add("back");
-            document
-              .querySelectorAll(".card")
-              [i].classList.remove("block_card");
+            document.querySelectorAll(".card")[i].classList.remove("block_card");
+
           }
+
         }
       }
 
       startGame = false;
 
-      let endGame = false;
+
+
 
       console.log(card.textContent);
     }
